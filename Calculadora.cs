c29084e6ca35cd29b0e10f2abe288a5ce55e8b1a
Ana Silva <anndasilva89@hotmail.com>
2020-03-25 20:59:20 -0300

    https://github.com/anasilva65/ProjectTestGithub

---
+++ Calculadora.cs
@@ -0,0 +1,20 @@
+using System;
+
+namespace aula1
+{
+    class aula1
+    {
+        static void Main(string[] args)
+        {
+            int A = 0
+            int B = 1
+            string operacao = "";
+
+            Console.WriteLine ("Por favor escolha a operacao ? \r\n" +
+            "a: Adicao \r\n" +
+            "b: Subtracao");
+            operacao = Console.ReadLine();
+
+            }
+            
+            
---
+++ Programddd.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace aula1
+{
+    class aula1
+    {
+        static void Main(string[] args)
+        {
+            //variaveis
+       string[] Telefones = new string[3] { "(11)992424940" , "(11)976991294" , "" };
+
+        // cadastro na lista
+        for(int indice = 0; indice <= 2; indice ++)
+        {
+         
+        Console.WriteLine(indice);
+        Console.WriteLine("Cadastre um telefone valido");
+        Telefones[indice] = Console.ReadLine();
+        }
+        foreach (string telefone in Telefones)
+
+
+        {
+            if(telefone.StartsWith("11"))  
+            Console.WriteLine(telefone + "e valido");
+        }
+
+        Console.ReadKey();
+        {
+            
+        }
+    }
+    
+}
+
+}
+
