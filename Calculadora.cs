64301e130264f600fdd45d8223d6d3db9cfad727
Ana Silva <anndasilva89@hotmail.com>
2020-03-25 21:14:15 -0300

    2 numeros e uma operacao

--- Calculadora.cs
+++ Calculadora.cs
@@ -10,10 +10,18 @@ namespace aula1
             int B = 1;
             string operacao = "";
 
-            Console.WriteLine ("Por favor escolha a operacao ? \r\n" +
-            "a: Adicao \r\n" +
-            "b: Subtracao");
+            Console.WriteLine ("Por favor digite o primeiro numero");
+            A = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine ("Por favor digite o segundo numero");
+            A = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine ("Por favor digite a opcao: \r\n" +
+            "a ou + : Adicao \r\n" +
+            "b ou - : Subt \r\n" +
+            "c ou x : Multip ");
             operacao = Console.ReadLine();
+            Console.ReadKey();
 
             }
 
